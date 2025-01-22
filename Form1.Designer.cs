c603ce527a175bb0017d053903cd29a29e5ff816
ralf <ralf@wrnt.de>
2025-01-22 21:16:42 +0100

    lieber auf deutsch + icon + readme

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace Trampolin.FilterSportTecResults
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tabs = new System.Windows.Forms.TabControl();
             this.tabInput = new System.Windows.Forms.TabPage();
             this.btnReadInput = new System.Windows.Forms.Button();
@@ -37,13 +38,13 @@ namespace Trampolin.FilterSportTecResults
             this.tabViewInput = new System.Windows.Forms.TabPage();
             this.gridInput = new System.Windows.Forms.DataGridView();
             this.tabFilter = new System.Windows.Forms.TabPage();
-            this.Output = new System.Windows.Forms.TabPage();
-            this.btnCreateOutputLtePlace = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.txtPlaceFilter = new System.Windows.Forms.TextBox();
             this.cmbClassFilter = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.txtPlaceFilter = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.btnCreateOutputGtPlace = new System.Windows.Forms.Button();
+            this.btnCreateOutputLtePlace = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.TabPage();
             this.txtOutput = new System.Windows.Forms.TextBox();
             this.tabs.SuspendLayout();
             this.tabInput.SuspendLayout();
@@ -78,7 +79,7 @@ namespace Trampolin.FilterSportTecResults
             this.tabInput.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabInput.Size = new System.Drawing.Size(880, 439);
             this.tabInput.TabIndex = 0;
-            this.tabInput.Text = "Input";
+            this.tabInput.Text = "Eingabe";
             this.tabInput.UseVisualStyleBackColor = true;
             // 
             // btnReadInput
@@ -88,7 +89,7 @@ namespace Trampolin.FilterSportTecResults
             this.btnReadInput.Name = "btnReadInput";
             this.btnReadInput.Size = new System.Drawing.Size(563, 85);
             this.btnReadInput.TabIndex = 2;
-            this.btnReadInput.Text = "Read";
+            this.btnReadInput.Text = "Datei einlesen";
             this.btnReadInput.UseVisualStyleBackColor = true;
             this.btnReadInput.Click += new System.EventHandler(this.btnReadInput_Click);
             // 
@@ -97,9 +98,9 @@ namespace Trampolin.FilterSportTecResults
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(38, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(126, 16);
+            this.label1.Size = new System.Drawing.Size(136, 16);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Input Csv File Name";
+            this.label1.Text = "Input Csv Datei Name";
             // 
             // txtInputFileName
             // 
@@ -122,7 +123,7 @@ namespace Trampolin.FilterSportTecResults
             this.tabViewInput.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tabViewInput.Size = new System.Drawing.Size(880, 439);
             this.tabViewInput.TabIndex = 1;
-            this.tabViewInput.Text = "View Input";
+            this.tabViewInput.Text = "Tabelle";
             this.tabViewInput.UseVisualStyleBackColor = true;
             // 
             // gridInput
@@ -158,35 +159,13 @@ namespace Trampolin.FilterSportTecResults
             this.tabFilter.Text = "Filter";
             this.tabFilter.UseVisualStyleBackColor = true;
             // 
-            // Output
+            // txtPlaceFilter
             // 
-            this.Output.Controls.Add(this.txtOutput);
-            this.Output.Location = new System.Drawing.Point(4, 25);
-            this.Output.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Output.Name = "Output";
-            this.Output.Size = new System.Drawing.Size(880, 439);
-            this.Output.TabIndex = 3;
-            this.Output.Text = "Output";
-            this.Output.UseVisualStyleBackColor = true;
-            // 
-            // btnCreateOutputLtePlace
-            // 
-            this.btnCreateOutputLtePlace.Location = new System.Drawing.Point(64, 172);
-            this.btnCreateOutputLtePlace.Name = "btnCreateOutputLtePlace";
-            this.btnCreateOutputLtePlace.Size = new System.Drawing.Size(562, 33);
-            this.btnCreateOutputLtePlace.TabIndex = 0;
-            this.btnCreateOutputLtePlace.Text = "Filter <= Platzierung";
-            this.btnCreateOutputLtePlace.UseVisualStyleBackColor = true;
-            this.btnCreateOutputLtePlace.Click += new System.EventHandler(this.btnCreateOutputLtePlace_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(61, 66);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 16);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Klasse";
+            this.txtPlaceFilter.Location = new System.Drawing.Point(150, 110);
+            this.txtPlaceFilter.Name = "txtPlaceFilter";
+            this.txtPlaceFilter.Size = new System.Drawing.Size(218, 22);
+            this.txtPlaceFilter.TabIndex = 3;
+            this.txtPlaceFilter.Text = "8";
             // 
             // cmbClassFilter
             // 
@@ -205,13 +184,14 @@ namespace Trampolin.FilterSportTecResults
             this.label3.TabIndex = 1;
             this.label3.Text = "Platzierung";
             // 
-            // txtPlaceFilter
+            // label2
             // 
-            this.txtPlaceFilter.Location = new System.Drawing.Point(150, 110);
-            this.txtPlaceFilter.Name = "txtPlaceFilter";
-            this.txtPlaceFilter.Size = new System.Drawing.Size(218, 22);
-            this.txtPlaceFilter.TabIndex = 3;
-            this.txtPlaceFilter.Text = "8";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Klasse";
             // 
             // btnCreateOutputGtPlace
             // 
@@ -222,6 +202,27 @@ namespace Trampolin.FilterSportTecResults
             this.btnCreateOutputGtPlace.Text = "Filter > Platzierung";
             this.btnCreateOutputGtPlace.UseVisualStyleBackColor = true;
             this.btnCreateOutputGtPlace.Click += new System.EventHandler(this.btnCreateOutputGtPlace_Click);
+            // 
+            // btnCreateOutputLtePlace
+            // 
+            this.btnCreateOutputLtePlace.Location = new System.Drawing.Point(64, 172);
+            this.btnCreateOutputLtePlace.Name = "btnCreateOutputLtePlace";
+            this.btnCreateOutputLtePlace.Size = new System.Drawing.Size(562, 33);
+            this.btnCreateOutputLtePlace.TabIndex = 0;
+            this.btnCreateOutputLtePlace.Text = "Filter <= Platzierung";
+            this.btnCreateOutputLtePlace.UseVisualStyleBackColor = true;
+            this.btnCreateOutputLtePlace.Click += new System.EventHandler(this.btnCreateOutputLtePlace_Click);
+            // 
+            // Output
+            // 
+            this.Output.Controls.Add(this.txtOutput);
+            this.Output.Location = new System.Drawing.Point(4, 25);
+            this.Output.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(880, 439);
+            this.Output.TabIndex = 3;
+            this.Output.Text = "Ausgabe";
+            this.Output.UseVisualStyleBackColor = true;
             // 
             // txtOutput
             // 
@@ -242,6 +243,7 @@ namespace Trampolin.FilterSportTecResults
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(888, 468);
             this.Controls.Add(this.tabs);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Filter SportsTec Result";
---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+﻿# Filter für Sporttech - Daten
+
+Lade Ergebnisse von der Website in CSV-Form herunter und speichere sie in einem Ordner.
+
+Ziehe per Maus die Datei in das Textfeld "Input Csv Datei Name" und klicke auf "Datei einlesen".
+
+Danach springt das Programm auf das 2. Tab und Zeigt die Daten an.
+
+Im Filter Tab kann man dann ein paar Parameter einstellen und die Daten filtern.
+
+Im letzten Tab steht dann der Text zum Kopieren für z.B. Excel bereit.
